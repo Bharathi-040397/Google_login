7ed90b9b777347e77a7d27a320013c2e4f3a191c
Bharathi <BharathiVairam040397@gmail.com>
2021-05-10 13:11:44 -0700

    second commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React,{useEffect} from 'react'
+import { BrowserRouter as Router, Route, Switch,useHistory,withRouter} from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import Login from '../Components/Login/Login'
 import Home from './Home/Home'
@@ -7,11 +8,16 @@ import Products from './Product/Products';
 import Cart from './Cart'
 import Checkout from './Checkout'
 
-
-
 function App() {
-  
+  const { user } = useSelector(state => state.user);
+  const history = useHistory();
 
+ useEffect(() => {
+  if (!user) {
+    window.location.pathnae="/"
+  }
+ }, [user])
+  
   return (
     <Router>
      
